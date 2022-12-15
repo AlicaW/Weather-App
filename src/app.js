487a4880b8536b8b349524e5973994ebeee6cd45
AlicaW <alica.wziontek@db-n.com>
2022-12-15 15:43:23 +0100

    updated weather icon

--- src/app.js
+++ src/app.js
@@ -29,17 +29,21 @@ function showTemperature(response) {
     let windElement = document.querySelector("#wind");
     let weatherElement = document.querySelector("#weather");
     let dateElement = document.querySelector("#current-time");
+    let iconElement = response.data.weather[0].icon;
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     weatherElement.innerHTML = response.data.weather[0].description;
     dateElement.innerHTML = formateDate(response.data.dt * 1000);
+    document.querySelector("#weatherIcon").innerHTML = `<img src="http://openweathermap.org/img/wn/${iconElement}@2x.png">`;
 }
 
 
     let apiKey = "597c40c39084687093b091cd48b366f8";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+    let city = "Hamburg";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     
     axios.get(apiUrl).then(showTemperature);
 
