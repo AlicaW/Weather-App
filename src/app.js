f05a5f6d24b562ad9f8de0841294f23c1ae071dc
AlicaW <alica.wziontek@db-n.com>
2022-12-15 15:24:59 +0100

    updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formateDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10 ) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#currentTemperature");
@@ -5,11 +29,13 @@ function showTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let weatherElement = document.querySelector("#weather");
+    let dateElement = document.querySelector("#current-time");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     weatherElement.innerHTML = response.data.weather[0].description;
+    dateElement.innerHTML = formateDate(response.data.dt * 1000);
 }
 
 
